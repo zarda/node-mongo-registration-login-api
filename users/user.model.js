40689c6451c0cc8d1ed0baf4b3c1241beb59a538
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2020-04-21 17:10:54 +0800

    change scheme items

--- users/user.model.js
+++ users/user.model.js
@@ -4,9 +4,8 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     username: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    createdDate: { type: Date, default: Date.now },
+    email: { type: String, required: true },
 });
 
 schema.set('toJSON', { virtuals: true });
